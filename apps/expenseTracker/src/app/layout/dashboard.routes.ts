42fbb9d8c4c031b3d77f8dbc870cf83d99c492ad
ahmed.gamal <ahmed.gamal@areebgroup.com>
2025-09-26 15:37:09 +0300

    feat: initialize home and expenses pages

--- apps/expenseTracker/src/app/layout/dashboard.routes.ts
+++ apps/expenseTracker/src/app/layout/dashboard.routes.ts
@@ -1,9 +1,24 @@
 import { Route } from '@angular/router';
+import { AppNavigation } from '../common/constants/app-navigation.constants';
 
 export const dashboardRoutes: Route[] = [
   {
-    path: '',
+    path: AppNavigation.home,
     pathMatch: 'full',
-    redirectTo: '',
+    redirectTo: AppNavigation.home,
+  },
+  {
+    path: AppNavigation.home,
+    loadComponent: () =>
+      import('../pages/dashboard/home/home.component').then(
+        (c) => c.HomeComponent
+      ),
+  },
+  {
+    path: AppNavigation.expenses,
+    loadComponent: () =>
+      import('../pages/dashboard/expenses/expenses.component').then(
+        (c) => c.ExpensesComponent
+      ),
   },
 ];
---
+++ apps/expenseTracker/src/app/pages/dashboard/expenses/expenses-form/expenses-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expenses-form',
+  imports: [],
+  templateUrl: './expenses-form.component.html',
+  styleUrl: './expenses-form.component.scss'
+})
+export class ExpensesFormComponent {
+
+}
